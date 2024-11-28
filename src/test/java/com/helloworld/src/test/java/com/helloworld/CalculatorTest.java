4e6f5b9b41905cf23e3adae2a560333901a3caa4
keerthikp <keerthi.kgp@gmail.com>
2024-11-28 12:25:37 +0530

    commit check

--- src/test/java/com/helloworld/src/test/java/com/helloworld/CalculatorTest.java
+++ src/test/java/com/helloworld/src/test/java/com/helloworld/CalculatorTest.java
@@ -3,7 +3,7 @@ package com.helloworld.src.test.java.com.helloworld;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.helloworld.Calculator; // test first commit // second commit
+import com.helloworld.Calculator; 
 
 public class CalculatorTest {
 
